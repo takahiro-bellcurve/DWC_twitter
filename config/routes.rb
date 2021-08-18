4abd14f15565ec4366230a6b94411111118b4d67
takahito-bellcurve <fukkuaaa@gmail.com>
2021-08-18 18:53:46 +0900

    kadai4(favorite-ajax)

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
+  devise_for :users
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update]
+end
